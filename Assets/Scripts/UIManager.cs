587a9d13298f647035ce634bfedfb31f9236b1b8
Vincent Beauzee <juanxt@gmail.com>
2019-01-26 00:28:21 +0100

    init UIManager and Servant

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private Servant currentServant;
+    public static GameObject INSTANCE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        INSTANCE = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void UpdateCurrentServant (Servant s) {
+        this.currentServant = s;
+    }
+
+    void ExecuteAction(GameObject interactiveTile) {
+        this.currentServant.ExecuteAction(interactiveTile);
+    }
+}

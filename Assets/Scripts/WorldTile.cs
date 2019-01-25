219032d60bc5c9fcb8d7c5c2399a1e3b521a3c5e
gtinon <guillaume.tinon@gmail.com>
2019-01-25 23:33:58 +0100

    Start working on pathfinding.

---
+++ Assets/Scripts/WorldTile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldTile : MonoBehaviour
+{
+
+    public int gridX;
+    public int gridY;
+    public bool walkable;
+    public List<WorldTile> myNeighbours;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

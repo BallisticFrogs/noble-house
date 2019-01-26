ae5ba00fa69329544364e67d45941401069d66a1
gtinon <guillaume.tinon@gmail.com>
2019-01-26 16:29:06 +0100

    Make a thingy move.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using SettlersEngine;
 
 public class GameController : MonoBehaviour
 {
+    public static GameController INSTANCE;
+
     public GameObject Grid;
+    public Tilemap tilemap;
     public PathNode[,] PathfindingMap;
 
     public SpatialAStar<PathNode, Character> aStar;
@@ -14,9 +18,19 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        Tilemap tilemap = Grid.GetComponent<Tilemap>();
+        INSTANCE = this;
+        tilemap = Grid.GetComponent<Tilemap>();
 
         BoundsInt bounds = tilemap.cellBounds;
+        // Debug.Log("bounds.x = " + bounds.x);
+        // Debug.Log("bounds.y = " + bounds.y);
+        // Debug.Log("bounds.xmax = " + bounds.xMax);
+        // Debug.Log("bounds.xmin = " + bounds.xMin);
+
+        // Debug.Log("0,0 = " + tilemap.GetCellCenterWorld(new Vector3Int(0, 0, 0)));
+        // Debug.Log("-1,-1 = " + tilemap.GetCellCenterWorld(new Vector3Int(-1, -1, 0)));
+        // Debug.Log("origin = " + tilemap.origin);
+
         TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
 
         // Declare Pathfinding Array
@@ -45,6 +59,12 @@ public class GameController : MonoBehaviour
         }
 
         aStar = new SpatialAStar<PathNode, Character>(PathfindingMap);
+    }
+
+    public LinkedList<PathNode> getPath(int x, int y, int targetX, int targetY)
+    {
+        LinkedList<PathNode> path = aStar.Search(new Point(x, y), new Point(targetX, targetY), null);
+        return path;
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Mobile.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mobile : MonoBehaviour
+{
+    public int targetX;
+    public int targetY;
+
+    public Vector3Int waypointCellCoords;
+
+    void Start()
+    {
+        Vector3Int cellCoords = GameController.INSTANCE.tilemap.WorldToCell(transform.position);
+        findNextWaypointToTarget(cellCoords);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3Int cellMouse = GameController.INSTANCE.tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            targetX = cellMouse.x;
+            targetY = cellMouse.y;
+        }
+
+        float threshold = 0.1f;
+        Vector3 waypointWorldCoords = GameController.INSTANCE.tilemap.GetCellCenterWorld(waypointCellCoords);
+        Vector3Int cellCoords = GameController.INSTANCE.tilemap.WorldToCell(transform.position);
+        if ((waypointWorldCoords - transform.position).magnitude < threshold)
+        {
+            findNextWaypointToTarget(cellCoords);
+        }
+        else if (cellCoords.x != targetX && cellCoords.y != targetY)
+        {
+            findNextWaypointToTarget(cellCoords);
+        }
+
+        Vector3 dist = waypointWorldCoords - transform.position;
+        if (dist.magnitude >= threshold * 0.1f)
+        {
+            Vector3 movement = dist.normalized * Math.Min(dist.magnitude, threshold);
+            transform.position = transform.position + movement;
+        }
+    }
+
+    private void findNextWaypointToTarget(Vector3Int currentCellCoords)
+    {
+        Debug.Log("Starting to pathfind...");
+        var cellBounds = GameController.INSTANCE.tilemap.cellBounds;
+        LinkedList<PathNode> path = GameController.INSTANCE.getPath(currentCellCoords.x - cellBounds.x, currentCellCoords.y - cellBounds.y,
+            targetX - cellBounds.x, targetY - cellBounds.y);
+
+        if (path != null && path.Count > 1)
+        {
+            PathNode waypointNode = path.First.Next.Value;
+            waypointCellCoords = new Vector3Int(waypointNode.X + cellBounds.x, waypointNode.Y + cellBounds.y, 0);
+            Debug.Log("found path : " + waypointNode.X + "-" + waypointNode.Y);
+        }
+        else
+         if (path != null && path.Count > 0)
+        {
+            PathNode waypointNode = path.First.Value;
+            waypointCellCoords = new Vector3Int(waypointNode.X + cellBounds.x, waypointNode.Y + cellBounds.y, 0);
+            Debug.Log("found path : " + waypointNode.X + "-" + waypointNode.Y);
+        }
+    }
+
+}

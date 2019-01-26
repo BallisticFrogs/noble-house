e3bdbc9e0005a0cad5a15a7daf4c6c594adf3682
Majimo <pierre.fervel@gmail.com>
2019-01-26 12:32:56 +0100

    Add Tiles to PathfindingMap

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,7 @@ using UnityEngine.Tilemaps;
 public class GameController : MonoBehaviour
 {
     public GameObject Grid;
+    public float[,] PathfindingMap;
 
     // Start is called before the first frame update
     void Awake()
@@ -15,17 +16,25 @@ public class GameController : MonoBehaviour
         BoundsInt bounds = tilemap.cellBounds;
         TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
 
+        // Declare Pathfinding Array
+        PathfindingMap = new float[bounds.size.x, bounds.size.y];
+
         // Recover Wall Tiles
         for (int x = 0; x < bounds.size.x; x++) {
+
             for (int y = 0; y < bounds.size.y; y++) {
                 TileBase tile = allTiles[x + y * bounds.size.x];
-                
-                if (tile != null) {
-                    if (tile.GetType() == typeof(WorldTile)) {
-                        WorldTile wt = tile as WorldTile;
-                        Debug.Log("x:" + x + " y:" + y + " tile:" + wt.Type);
-                    }                    
-                }
+
+                if (tile != null && tile.GetType() == typeof(WorldTile)) {
+                    WorldTile wt = tile as WorldTile;
+
+                    // Player can't walk in
+                    PathfindingMap[x, y] = 0;
+                }  
+                else {
+                    // Player can walk
+                    PathfindingMap[x, y] = 1;
+                }   
             }
         }
     }

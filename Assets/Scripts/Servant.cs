9eca5914a57fc78f8322ea8d5ebec75097cc4acf
Majimo <pierre.fervel@gmail.com>
2019-01-26 17:18:54 +0100

    Add Well to Objects TileMap

--- Assets/Scripts/Servant.cs
+++ Assets/Scripts/Servant.cs
@@ -15,7 +15,25 @@ public class Servant : Character
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonUp(0))
+        {
+            // Coord dans la grille
+            Vector3Int cellMouse = GameController.INSTANCE.tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            var tileClicked = GameController.INSTANCE.tilemap.GetTile(cellMouse);
+
+            if (tileClicked != null && tileClicked.GetType() == typeof(WorldTile))
+            {
+                WorldTile wt = tileClicked as WorldTile;
+                if (wt.tileType == TileType.WELL)
+                {
+                    Debug.Log("Clic sur le puits");
+                }
+                else
+                {
+                    Debug.Log("Clic ailleurs, dommage");
+                }
+            }
+        }
     }
 
     void OnMouseUp()
--- Assets/Scripts/TileType.cs
+++ Assets/Scripts/TileType.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 public enum TileType
 {
     CHARACTER,
-    WALL
+    WALL,
+    WELL
 }

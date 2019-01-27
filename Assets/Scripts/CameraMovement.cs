b17511c5f8ca00827c26fbbc987f94b7b8bdb024
Majimo <pierre.fervel@gmail.com>
2019-01-27 16:04:02 +0100

    Limit Camera movement when out of display

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -13,38 +13,41 @@ public class CameraMovement : MonoBehaviour
     
     void Update()
     {
+        // x: -20.5 / 7.5
+        // y: -15 / 21.1
+
         // Move camera with arrow keys
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < -1)
         {
             transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -14)
         {
             transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) && transform.position.y > -15)
         {
             transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) && transform.position.y < 21)
         {
             transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
         }
-        
+
         // Move camera when mouse on the edge of the screen
-        if (Input.mousePosition.x >= Screen.width - margin)
+        if (Input.mousePosition.x >= (Screen.width - margin) && transform.position.x < -1)
         {
             transform.position += mRightDirection * Time.deltaTime * speed;
         }
-        if (Input.mousePosition.x <= margin)
+        if (Input.mousePosition.x <= margin && transform.position.x > -14)
         {
             transform.position += mLeftDirection * Time.deltaTime * speed;
         }
-        if (Input.mousePosition.y >= Screen.height - margin)
+        if (Input.mousePosition.y >= (Screen.height - margin) && transform.position.y < 21)
         {
             transform.position += mUpDirection * Time.deltaTime * speed;
         }
-        if (Input.mousePosition.y <= margin)
+        if (Input.mousePosition.y <= margin && transform.position.y > -15)
         {
             transform.position += mDownDirection * Time.deltaTime * speed;
         }
